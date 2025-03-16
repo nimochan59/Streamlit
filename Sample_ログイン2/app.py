343a8c918b19c9e091ba41a88c743d4a6506d61d
nimochan59 <kento.niimoto@gmail.com>
2025-03-16 22:16:17 +0900

    ログイン機能訂正

---
+++ Sample_ログイン2/app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days']
+)
+
+authenticator.login()
+
+if st.session_state['authentication_status']:
+    authenticator.logout()
+    st.write(f'ようこそ *{st.session_state["name"]}* さん')
+    st.title('コンテンツ')
+elif st.session_state['authentication_status'] is False:
+    st.error('ユーザー名またはパスワードが正しくありません')
+elif st.session_state['authentication_status'] is None:
+    st.warning('ユーザー名とパスワードを入力してください')
